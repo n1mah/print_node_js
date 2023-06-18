ad9c83376b5ad02fc9c45ac167ccc7e7e911feee
n1mah <nima.heidari.n@gmail.com>
2023-06-18 23:42:18 +0330

    Test 2 Level Show Node Manual

--- js/main.js
+++ js/main.js
@@ -1,33 +1,51 @@
+let node = {
+    A: {
+       B:{
+           D:{
+               J:{
+                   R:null
+               },
+               K:null
+           },
+           E:{
+               L:null,
+               M:null
+           },
+           F:{
+               N:null,
+               O:null
+           },
+       },
+       C:{
+           G:{
+               P:null,
+           },
+           H:{
+               Q:null,
+           },
+           I:null,
+       },
+    },
+};
 
-    class Node
-    {
-        constructor(data)
-    {
-        this.left=this.right=null;
-        this.data=data;
+// console.log(node)
+// node.map()
+// node.forEach(()=>{
+//     console.log(value);
+// });
+
+
+// Test 2 Level Show Node Manual
+for (const key in node) {
+    console.log(key)
+    if (node[key]){
+        console.log(node[key])
+        for (const key2 in node[key]){
+            console.log(node[key][key2])
+        }
+    }else {
+        console.log(key)
     }
-    }
-
-    function findFullNode(root)
-    {
-        if (root != null)
-    {
-        findFullNode(root.left);
-        if (root.left != null && root.right != null)
-        document.write(root.data+" ");
-        findFullNode(root.right);
-    }
-    }
-
-    let root = new Node(1);
-    root.left = new Node(2);
-    root.right = new Node(3);
-    root.left.left = new Node(4);
-    root.right.left = new Node(5);
-    root.right.right = new Node(6);
-    root.right.left.right = new Node(7);
-    root.right.right.right = new Node(8);
-    root.right.left.right.left = new Node(9);
-    findFullNode(root);
-
-
+//     if ()
+//     console.log(`${key}: ${node[key]}`);
+}
