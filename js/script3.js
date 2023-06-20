07213a17838f80eaaa57216219404c1418b2c4a3
n1mah <nima.heidari.n@gmail.com>
2023-06-20 20:00:26 +0330

    Complete Print Node In Window

--- js/script3.js
+++ js/script3.js
@@ -59,7 +59,6 @@ const printTree = (node) => {
                 left
                     ? `
                 <div class="node left">
-                <canvas class="canvas canvas-left"> </canvas>
                   ${printTree(left)}
                 </div>
                 `
@@ -69,7 +68,6 @@ const printTree = (node) => {
                 right
                     ? `
                 <div class="node right">
-                <canvas class="canvas canvas-right"> </canvas>
                   ${printTree(right)}
                 </div>
                 `
@@ -102,28 +100,94 @@ main();
 // console.log("E-"+document.querySelector("#E").getBoundingClientRect().left)
 // console.log("F-"+document.querySelector("#F").getBoundingClientRect().left)
 // console.log("G-"+document.querySelector("#G").getBoundingClientRect().left)
-document.querySelectorAll(".canvas-right").forEach(value => {
-    ctx = value.getContext('2d');
-    drawLine(ctx, 0, 0, 30, 30, 'blue', 3);
-})
+// document.querySelectorAll(".canvas-right").forEach(value => {
+//     ctx = value.getContext('2d');
+//     drawLine(ctx, 0, 0, 30, 30, 'blue', 3);
+// })
 
-document.querySelectorAll(".canvas-left").forEach(value => {
-    ctx = value.getContext('2d');
-    drawLine(ctx, 0, 0, 30, 30, 'red', 3);
-})
-
-// let canvas = document.querySelector('.canvas'),
-//     ctx = canvas.getContext('2d');
-// drawLine(ctx, 0, 0, 30, 30, 'blue', 3);
+// document.querySelectorAll(".canvas-left").forEach(value => {
+//     ctx = value.getContext('2d');
+//     drawLine(ctx, 0, 0, 30, 30, 'red', 3);
+// })
 
 let positionMade=[];
-const ddd = (node,parent=null) => {
+const ddd = (node,parent=null,px=null,py=null) => {
     const { left, right, value } = node;
-    positionMade.push([value,document.querySelector("#"+value).getBoundingClientRect().left,document.querySelector("#"+value).getBoundingClientRect().top,parent]);
+    // if (parent!==null && px!==null && py!==null){
+    //
+    // }
+    positionMade.push([value,document.querySelector("#"+value).getBoundingClientRect().left,document.querySelector("#"+value).getBoundingClientRect().top,parent,px,py]);
     if (left!=null)
-        (ddd(left,value))
+        (ddd(left,value,document.querySelector("#"+value).getBoundingClientRect().left,document.querySelector("#"+value).getBoundingClientRect().top))
     if (right!=null)
-        (ddd(right,value))
+        (ddd(right,value,document.querySelector("#"+value).getBoundingClientRect().left,document.querySelector("#"+value).getBoundingClientRect().top))
     return positionMade;
 }
 console.log(ddd(data));
+
+positionMade.forEach((value,index) => {
+    let width=25;
+    let height=25;
+    // console.log(positionMade)
+    if (positionMade[index][3]!=null){
+        document.querySelector("#canvases").appendChild(createLine(positionMade[index][4]+width, positionMade[index][5]+height, positionMade[index][1]+width, positionMade[index][2]+height))
+
+        // document.querySelector("#canvases").innerHTML+=`
+        //         <canvas class="canvas canvas-${positionMade[index][0]}"> </canvas>
+        // `;
+
+        // let el=document.querySelector(".canvas-"+positionMade[index][0])
+        // console.log(el)
+        // let ctx = el.getContext('2d');
+        // console.log(positionMade[index][4], positionMade[index][5] , positionMade[index][1], positionMade[index][2])
+        // console.log(positionMade[index-1][2])
+        // console.log(positionMade[index][1])
+        // console.log(positionMade[index][2])
+        // drawLine(ctx, positionMade[index][4], positionMade[index][5] , positionMade[index][1], positionMade[index][2], 'blue', 3);
+        // console.log(positionMade[index][0])
+    }
+})
+
+//
+// document.querySelector("#canvases").innerHTML+=`<canvas class="canvas1"> </canvas>`;
+//
+// let canvas = document.querySelector('.canvas1'),
+//     ctx = canvas.getContext('2d');
+// drawLine(ctx, 300, 0, 600, 900, 'blue', 20);
+//
+//
+
+function createLineElement(x, y, length, angle) {
+    var line = document.createElement("div");
+    var styles = 'border: 1px solid black; '
+        + 'width: ' + length + 'px; '
+        + 'height: 0px; '
+        + '-moz-transform: rotate(' + angle + 'rad); '
+        + '-webkit-transform: rotate(' + angle + 'rad); '
+        + '-o-transform: rotate(' + angle + 'rad); '
+        + '-ms-transform: rotate(' + angle + 'rad); '
+        + 'position: absolute; '
+        + 'top: ' + y + 'px; '
+        + 'left: ' + x + 'px; ';
+    line.setAttribute('style', styles);
+    return line;
+}
+
+function createLine(x1, y1, x2, y2) {
+    var a = x1 - x2,
+        b = y1 - y2,
+        c = Math.sqrt(a * a + b * b);
+
+    var sx = (x1 + x2) / 2,
+        sy = (y1 + y2) / 2;
+
+    var x = sx - c / 2,
+        y = sy;
+
+    var alpha = Math.PI - Math.atan2(-b, a);
+
+    return createLineElement(x, y, c, alpha);
+}
+
+// document.body.appendChild(createLine(100, 100, 200, 200));
+
